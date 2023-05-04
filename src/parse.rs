d9d362e9aaebef076e8fbedb2b6323d09bc63460
Yota Toyama <raviqqe@gmail.com>
2023-05-04 21:25:48 +1000

    Fix

--- src/parse.rs
+++ src/parse.rs
@@ -1,5 +1,6 @@
 use crate::expression::Expression;
+use tokio::io::AsyncRead;
 
-pub async fn parse_expression() -> Expression {
+pub async fn parse_expression(reader: impl AsyncRead) -> Expression {
     todo!()
 }
