4d688c99c318013bf01ecc4f6881a8fda847de96
Yota Toyama <raviqqe@gmail.com>
2023-05-04 21:21:17 +1000

    Fix

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-enum Expression { 
-    Symbol(foo),
+#[derive(Debug, Default)]
+enum Expression {
+    #[default]
+    None,
+    Symbol(String),
 }
 
 fn main() {
     println!("Hello, world!");
 }
 
-fn parse_expression(foo) -> foo  {
-
+fn parse_expression() -> Expression {
+    todo!()
 }
