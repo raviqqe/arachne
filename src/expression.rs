fa629176b7f5b216fbdd10919982a12308031365
Yota Toyama <raviqqe@gmail.com>
2023-05-05 15:54:50 +1000

    Fix

--- src/expression.rs
+++ src/expression.rs
@@ -1,3 +1,5 @@
+use std::fmt::{self, Display, Formatter};
+
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum Expression {
     Symbol(String),
@@ -19,5 +21,24 @@ impl From<String> for Expression {
 impl From<Vec<Expression>> for Expression {
     fn from(array: Vec<Expression>) -> Self {
         Expression::Array(array)
+    }
+}
+
+impl Display for Expression {
+    fn fmt(&self, formatter: &mut Formatter) -> fmt::Result {
+        match self {
+            Expression::Symbol(symbol) => write!(formatter, "{}", symbol),
+            Expression::Array(array) => {
+                write!(formatter, "(")?;
+
+                for expression in array {
+                    write!(formatter, "{}", expression)?;
+                }
+
+                write!(formatter, ")")?;
+
+                Ok(())
+            }
+        }
     }
 }
