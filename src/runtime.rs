349081530564060e95eace0626bcc5477f903bcc
Yota Toyama <raviqqe@gmail.com>
2023-05-06 15:49:15 +1000

    Implement methods

--- src/runtime.rs
+++ src/runtime.rs
@@ -1,1 +1,25 @@
 pub type Value = f64;
+
+#[repr(transparent)]
+pub struct Number(Value);
+
+#[repr(transparent)]
+pub struct Array(f64);
+
+impl Array {
+    pub fn new() -> Self {
+        Self(0.0)
+    }
+
+    pub fn get(&self, index: Value) -> Value {
+        todo!()
+    }
+
+    pub fn set(&self, index: Value, value: Value) -> Value {
+        todo!()
+    }
+
+    pub fn len(&self) -> Value {
+        todo!()
+    }
+}
