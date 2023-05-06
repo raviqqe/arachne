ff36b0bb3a6bdbf6e422b8b43e127d586f888acf
Yota Toyama <raviqqe@gmail.com>
2023-05-06 16:22:40 +1000

    Fix

--- src/runtime/array.rs
+++ src/runtime/array.rs
@@ -1,10 +1,26 @@
 use super::Value;
+use std::{
+    alloc::{alloc, Layout},
+    mem::size_of,
+};
+
+const ALIGNMENT: usize = 8;
 
 pub struct Array(u64);
 
+struct Header {
+    count: usize,
+    length: usize,
+}
+
 impl Array {
-    pub fn new() -> Self {
-        todo!()
+    pub fn new(size: usize) -> Self {
+        let layout = Layout::new::<Header>()
+            .extend(Layout::from_size_align(size_of::<usize>() * size, ALIGNMENT).unwrap())
+            .unwrap()
+            .0;
+
+        Self(unsafe { alloc(layout) } as usize as u64 & (1 << 63))
     }
 
     pub fn get(&self, index: Value) -> Value {
