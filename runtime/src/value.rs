430ec1c9453648e0e453bf646639acc157c53f09
Yota Toyama <raviqqe@gmail.com>
2023-05-07 14:13:09 +1000

    Test NaN (#32)

--- runtime/src/value.rs
+++ runtime/src/value.rs
@@ -2,7 +2,7 @@ use super::{Array, Number};
 
 pub const NIL: Value = Value(0);
 const EXPONENT_MASK: u64 = 0x7ff0_0000_0000_0000;
-pub const ARRAY_MASK: u64 = 0x1 | EXPONENT_MASK;
+pub const ARRAY_MASK: u64 = 0x0004_0000_0000_0000 | EXPONENT_MASK;
 
 #[derive(Debug)]
 pub struct Value(u64);
@@ -99,7 +99,22 @@ mod tests {
 
     #[test]
     fn nil() {
-        let _ = NIL;
+        assert_eq!(NIL, Value::from(0.0));
+    }
+
+    #[test]
+    fn nan() {
+        assert!(Value::from(f64::NAN).is_number());
+        assert!(Value::from(-f64::NAN).is_number());
+        assert!(Value::from(-0.0 / 0.0).is_number());
+    }
+
+    #[test]
+    fn zero_division() {
+        assert!(Value::from(-0.0).is_number());
+        assert!(Value::from(-1.0).is_number());
+        assert!(Value::from(1.0 / 0.0).is_number());
+        assert!(Value::from(-1.0 / 0.0).is_number());
     }
 
     mod clone {
