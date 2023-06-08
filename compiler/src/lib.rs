eca632c7ccaab0fb940ccb61d6c31cf8d82e2029
Yota Toyama <raviqqe@gmail.com>
2023-06-08 14:50:36 +1000

    Fix

--- compiler/src/lib.rs
+++ compiler/src/lib.rs
@@ -88,6 +88,6 @@ fn compile_variable(_symbol: Symbol, codes: &mut Vec<u8>) {
 }
 
 fn compile_call(_array: Array, codes: &mut Vec<u8>) {
-    todo!()
+    compile_arguments(array, 3, codes);
     codes.push(Instruction::Call as u8);
 }
