088bace1e5a645c2062f10e8f00586ccb2801ea6
Yota Toyama <raviqqe@gmail.com>
2023-06-08 13:57:56 +1000

    Add compiler crate (#68)

--- command/src/main.rs
+++ command/src/main.rs
@@ -3,6 +3,7 @@ use parse::parse;
 use std::error::Error;
 use tokio::io::{stdin, AsyncBufReadExt, BufReader};
 use tokio_stream::wrappers::LinesStream;
+use vm_interpreter::Interpreter;
 
 macro_rules! interpret_fn {
     ($name: ident, $stream_fn: expr) => {
@@ -25,6 +26,8 @@ macro_rules! interpret_fn {
     };
 }
 
+const BYTE_CODE_CAPACITY: usize = 1 << 10;
+
 interpret_fn!(interpret_naive, naive_interpreter::interpret);
 interpret_fn!(interpret_mlir, mlir_interpreter::interpret);
 
@@ -38,13 +41,35 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 .help("Use naive implementation")
                 .action(clap::ArgAction::SetTrue),
         )
+        .arg(
+            clap::Arg::new("mlir")
+                .long("mlir")
+                .help("Use mlir implementation")
+                .action(clap::ArgAction::SetTrue),
+        )
         .get_matches();
 
     if matches.subcommand().is_some() {
         todo!("format subcommand")
     } else if matches.get_one("naive").copied().unwrap_or_default() {
         interpret_naive().await
-    } else {
+    } else if matches.get_one("mlir").copied().unwrap_or_default() {
         interpret_mlir().await
+    } else {
+        let mut lines = LinesStream::new(BufReader::new(stdin()).lines());
+        let values = parse(&mut lines);
+
+        pin_mut!(values);
+
+        let interpreter = Interpreter::new(BYTE_CODE_CAPACITY);
+        let outputs = interpreter.interpret(&mut values);
+
+        pin_mut!(outputs);
+
+        while let Some(result) = outputs.next().await {
+            println!("{}", result?);
+        }
+
+        Ok(())
     }
 }
---
+++ interpreter/vm/src/lib.rs
@@ -0,0 +1,42 @@
+mod error;
+
+use async_stream::try_stream;
+use compiler::Compiler;
+use error::InterpretError;
+use futures::{pin_mut, Stream, StreamExt};
+use runtime::{Value, NIL};
+use std::{cell::RefCell, error::Error};
+use vm::Vm;
+
+const VM_STACK_SIZE: usize = 1 << 10;
+
+pub struct Interpreter {
+    codes: RefCell<Vec<u8>>,
+}
+
+impl Interpreter {
+    pub fn new(byte_code_capacity: usize) -> Self {
+        Self {
+            codes: Vec::with_capacity(byte_code_capacity).into(),
+        }
+    }
+
+    pub fn interpret<'a, E: Error + 'static>(
+        &'a self,
+        values: &'a mut (impl Stream<Item = Result<Value, E>> + Unpin),
+    ) -> impl Stream<Item = Result<Value, InterpretError>> + 'a {
+        try_stream! {
+            let compiler = Compiler::new(&self.codes);
+            let mut vm = Vm::new(VM_STACK_SIZE);
+            let results = compiler.compile(values);
+
+            pin_mut!(results);
+
+            while let Some(result) = results.next().await {
+                result.map_err(|error| InterpretError::Other(error.into()))?;
+                vm.run(&self.codes.borrow());
+                yield NIL;
+            }
+        }
+    }
+}
