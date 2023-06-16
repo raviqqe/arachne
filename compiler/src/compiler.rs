c0932c9830bf13df21a84037e9257d2cee3ad25e
Yota Toyama <raviqqe@gmail.com>
2023-06-16 20:06:13 +1000

    Fix

--- compiler/src/compiler.rs
+++ compiler/src/compiler.rs
@@ -19,6 +19,7 @@ impl<'a> Compiler<'a> {
         values: &'a mut (impl Stream<Item = Result<Value, E>> + Unpin),
     ) -> impl Stream<Item = Result<(), CompileError>> + 'a {
         try_stream! {
+            // TODO Set a global flag.
             let function = Function::new();
             let mut block = Block::new(&function);
 
