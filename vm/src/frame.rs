cd6d674bb6a49e98ee0a578c61a31aed5e31e531
Yota Toyama <raviqqe@gmail.com>
2023-06-16 21:07:15 +1000

    Fix

--- vm/src/frame.rs
+++ vm/src/frame.rs
@@ -1,22 +1,22 @@
 pub struct Frame {
-    return_address: u32,
     pointer: u32,
+    return_address: u32,
 }
 
 impl Frame {
     // TODO Swap the argument order.
-    pub fn new(return_address: u32, frame_pointer: u32) -> Self {
+    pub fn new(return_address: u32, pointer: u32) -> Self {
         Self {
+            pointer,
             return_address,
-            pointer: frame_pointer,
         }
-    }
-
-    pub fn return_address(&self) -> u32 {
-        self.return_address
     }
 
     pub fn pointer(&self) -> u32 {
         self.pointer
+    }
+
+    pub fn return_address(&self) -> u32 {
+        self.return_address
     }
 }
