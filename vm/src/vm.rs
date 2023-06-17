d05af72730c37ea33d66aa84a11e0d9a910fbc2b
Yota Toyama <raviqqe@gmail.com>
2023-06-17 17:16:48 +1000

    Fix

--- vm/src/vm.rs
+++ vm/src/vm.rs
@@ -62,7 +62,34 @@ impl Vm {
                 Instruction::Jump => Self::jump,
                 Instruction::Branch => Self::branch,
                 Instruction::Return => Self::r#return,
-            })(codes)
+            })(self, codes)
+        }
+    }
+
+    fn get_instruction(&mut self, codes: &[u8]) -> fn(&mut Vm, &[u8]) {
+        match Instruction::from_u8(self.read_u8(codes)).expect("valid instruction") {
+            Instruction::Nil => Self::nil,
+            Instruction::Float64 => Self::float64,
+            Instruction::Integer32 => Self::integer32,
+            Instruction::Symbol => Self::symbol,
+            Instruction::Get => Self::get,
+            Instruction::Set => Self::set,
+            Instruction::Length => Self::length,
+            Instruction::Add => Self::add,
+            Instruction::Subtract => Self::subtract,
+            Instruction::Multiply => Self::multiply,
+            Instruction::Divide => Self::divide,
+            Instruction::Drop => Self::drop,
+            Instruction::Dump => Self::dump,
+            Instruction::Call => Self::call,
+            Instruction::TailCall => Self::tail_call,
+            Instruction::Close => Self::close,
+            Instruction::Environment => Self::environment,
+            Instruction::Peek => Self::peek,
+            Instruction::Equal => Self::equal,
+            Instruction::Jump => Self::jump,
+            Instruction::Branch => Self::branch,
+            Instruction::Return => Self::r#return,
         }
     }
 
