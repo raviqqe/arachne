6e9d08f1ff4b4ad47eb016636d090d781785109d
Yota Toyama <raviqqe@gmail.com>
2023-06-20 13:38:53 +1000

    Fix

--- bench/fibonacci/main.sh
+++ bench/fibonacci/main.sh
@@ -4,4 +4,4 @@ set -e
 
 cd $(dirname $0)
 
-hyperfine '../../target/release/arachne < main.arc' 'python3 main.py'
+hyperfine '../../target/release/arachne < main.arc' 'python3 main.py' 'csi -s main.scm'
