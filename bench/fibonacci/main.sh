366f21defae12844159f24a2365d4ee33839e029
Yota Toyama <raviqqe@gmail.com>
2023-06-20 23:50:17 +1000

    Install Chez Scheme

--- bench/fibonacci/main.sh
+++ bench/fibonacci/main.sh
@@ -4,4 +4,4 @@ set -e
 
 cd $(dirname $0)
 
-hyperfine '../../target/release/arachne < main.arc' 'python3 main.py' 'csi -s main.scm'
+hyperfine '../../target/release/arachne < main.arc' 'python3 main.py' 'petite --script main.scm'
