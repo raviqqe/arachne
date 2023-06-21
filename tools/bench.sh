1b4fa274a950e1263efc721dccb48fee2651be44
Yota Toyama <raviqqe@gmail.com>
2023-06-21 13:42:46 +1000

    Remove main.sh

--- tools/bench.sh
+++ tools/bench.sh
@@ -2,9 +2,20 @@
 
 set -e
 
+cd $(dirname $0)/..
+
 cargo build --release
 cargo install hyperfine
 
 for directory in fibonacci sum tak; do
-  bench/$directory/main.sh
+  directory=bench/$directory
+
+  if which petite >/dev/null; then
+    scheme="petite --script $directory/main.scm"
+  fi
+
+  hyperfine --sort command -w 2 \
+    "target/release/arachne < $directory/main.arc" \
+    "python3 $directory/main.py" \
+    $scheme
 done
