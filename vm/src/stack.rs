0c3afa3ad5ae0b2be601faefa2bd056f51ceb59e
Yota Toyama <raviqqe@gmail.com>
2023-06-21 15:26:49 +1000

    Fixed-size stack (#141)

--- vm/src/stack.rs
+++ vm/src/stack.rs
@@ -1,36 +1,141 @@
-use runtime::Value;
+use runtime::{Value, NIL};
+use std::ptr::{copy, read, write};
 
-#[derive(Debug, Default)]
+const SIZE: usize = 1 << 11;
+
+#[derive(Debug)]
 pub struct Stack {
-    values: Vec<Value>,
+    base: *mut Value,
+    pointer: *mut Value,
 }
 
 impl Stack {
     pub fn new() -> Self {
-        Self::default()
+        let values = Box::<[Value]>::leak(vec![NIL; SIZE].into());
+
+        Self {
+            base: &mut values[0],
+            pointer: &mut values[0],
+        }
     }
 
+    #[inline(always)]
     pub fn push(&mut self, value: Value) {
-        self.values.push(value);
+        if self.len() >= SIZE {
+            panic!("stack overflow");
+        }
+
+        unsafe {
+            write(self.pointer, value);
+        }
+
+        self.pointer = unsafe { self.pointer.add(1) };
     }
 
+    #[inline(always)]
     pub fn pop(&mut self) -> Value {
-        self.values.pop().expect("stack value")
+        self.pointer = unsafe { self.pointer.sub(1) };
+
+        unsafe { read(self.pointer) }
     }
 
+    #[inline(always)]
     pub fn peek(&self, index: usize) -> &Value {
-        self.values.get(self.get_index(index)).unwrap()
+        unsafe { &*self.pointer.sub(index + 1) }
     }
 
+    #[inline(always)]
     pub fn truncate(&mut self, start: usize, end: usize) {
-        self.values.splice(start..end, []);
+        for index in start..end {
+            unsafe { read(self.base.add(index)) };
+        }
+
+        let count = self.len() - end;
+
+        unsafe {
+            copy(self.base.add(end), self.base.add(start), count);
+
+            self.pointer = self.base.add(start).add(count);
+        };
     }
 
+    #[inline(always)]
     pub fn len(&self) -> usize {
-        self.values.len()
+        (unsafe { self.pointer.offset_from(self.base) }) as usize
+    }
+}
+
+impl Default for Stack {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn push_and_pop_scalars() {
+        let mut stack = Stack::new();
+        stack.push(1.0.into());
+        stack.push(2.0.into());
+
+        assert_eq!(stack.pop(), 2.0.into());
+        assert_eq!(stack.pop(), 1.0.into());
     }
 
-    fn get_index(&self, index: usize) -> usize {
-        self.values.len() - 1 - index
+    #[test]
+    fn push_and_pop_containers() {
+        let mut stack = Stack::new();
+        stack.push([1.0.into()].into());
+        stack.push([2.0.into()].into());
+
+        assert_eq!(stack.pop(), [2.0.into()].into());
+        assert_eq!(stack.pop(), [1.0.into()].into());
+    }
+
+    #[test]
+    fn peek() {
+        let mut stack = Stack::new();
+        stack.push(1.0.into());
+        stack.push(2.0.into());
+
+        assert_eq!(stack.peek(0), &2.0.into());
+        assert_eq!(stack.peek(1), &1.0.into());
+    }
+
+    #[test]
+    fn truncate() {
+        let mut stack = Stack::new();
+        stack.push(1.0.into());
+        stack.push(2.0.into());
+
+        stack.truncate(0, 1);
+
+        assert_eq!(stack.pop(), 2.0.into());
+    }
+
+    #[test]
+    fn truncate_overlapping() {
+        let mut stack = Stack::new();
+        stack.push(1.0.into());
+        stack.push(2.0.into());
+        stack.push(3.0.into());
+
+        stack.truncate(0, 1);
+
+        assert_eq!(stack.pop(), 3.0.into());
+        assert_eq!(stack.pop(), 2.0.into());
+    }
+
+    #[test]
+    #[should_panic]
+    fn overflow() {
+        let mut stack = Stack::new();
+
+        for _ in 0..SIZE + 1 {
+            stack.push(NIL);
+        }
     }
 }
