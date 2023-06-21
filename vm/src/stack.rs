01a259a75ebf10a48c2bed48d2b8c068cfa4b293
Yota Toyama <raviqqe@gmail.com>
2023-06-21 14:31:36 +1000

    Fix

--- vm/src/stack.rs
+++ vm/src/stack.rs
@@ -19,6 +19,7 @@ impl Stack {
         }
     }
 
+    #[inline]
     pub fn push(&mut self, value: Value) {
         if self.len() >= SIZE {
             panic!("stack overflow");
@@ -31,16 +32,19 @@ impl Stack {
         self.pointer = unsafe { self.pointer.add(1) };
     }
 
+    #[inline]
     pub fn pop(&mut self) -> Value {
         self.pointer = unsafe { self.pointer.sub(1) };
 
         unsafe { read(self.pointer) }
     }
 
+    #[inline]
     pub fn peek(&self, index: usize) -> &Value {
         unsafe { &*self.pointer.sub(index + 1) }
     }
 
+    #[inline]
     pub fn truncate(&mut self, start: usize, end: usize) {
         for index in start..end {
             unsafe { read(self.base.add(index)) };
@@ -55,6 +59,7 @@ impl Stack {
         };
     }
 
+    #[inline]
     pub fn len(&self) -> usize {
         (unsafe { self.pointer.offset_from(self.base) }) as usize
     }
